abd57797d42be32a3c634f1564968583182ecc89
jona8690 <jona8690@edu.eal.dk>
2017-03-02 08:46:06 +0100

    Added core project

---
+++ Core/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core {
+	class Player {
+		public char Symbol { get; private set; }
+		public string Name { get; private set; }
+		public int Life { get; private set; }
+
+	}
+}
--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI.Console")]
+[assembly: AssemblyTitle("Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UI.Console")]
+[assembly: AssemblyProduct("Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc9a85ea-2921-4cf2-a925-416b687a7383")]
+[assembly: Guid("d42e1c51-0af1-4ea6-94fa-c8ff72e12cb5")]
 
 // Version information for an assembly consists of the following four values:
 //
